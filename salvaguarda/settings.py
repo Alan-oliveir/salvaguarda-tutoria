b5db9f5e5aee559acd5b2b9ebb4d8c565b9e62ed
Alan <alandeog@poli.ufrj.br>
2026-06-29 19:00:56 -0300

    feat: add CSRF_TRUSTED_ORIGINS setting to enhance security

--- salvaguarda/settings.py
+++ salvaguarda/settings.py
@@ -23,6 +23,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=[])
 
 # Application definition
 
